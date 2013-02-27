a17647aaf38023142f7ffe354dfcc0b4dce3b7d7
Leander Herzog <info@leanderherzog.ch>
2013-02-27 16:59:21 +0100

    setup

--- config.rb
+++ config.rb
@@ -1,12 +1,10 @@
-require 'html5-boilerplate'
-require 'ninesixty'
+require "zurb-foundation"
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css/"
-sass_dir = "src"
+css_dir = "css"
+sass_dir = "css"
 images_dir = "images"
 javascripts_dir = "js"
 output_style = :compressed
-# To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 preferred_syntax = :sass
